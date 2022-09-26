7e17b6b1c14df51144000bc1fb5426bb18ebd9c2
Dilan Grajales <193075@uptapachula.edu.mx>
2022-09-25 21:27:27 -0500

    quitar serializers

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,17 +1,5 @@
-from django.urls import path, include
-from rest_framework import routers
-from .views import LoginView, RegisterView
-from apps.usuarios import views
-
-router = routers.DefaultRouter()
-
-router.register(r'agente', views.AgenteViewSet)
+from django.urls import path
 
 urlpatterns = [
-    path('api/', include(router.urls)),    
-        # Auth views
-    path('api/auth/login/',LoginView.as_view(), name='auth_login'),
-    path('api/register/', RegisterView.as_view(), name='auth_register'),
-
-    # path('auth/logout/',LogoutView.as_view(), name='auth_logout'),
+    
 ]
