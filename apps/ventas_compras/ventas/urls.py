318b33e695a368e6ecce680da7e0b6236c0565ec
loyfi <nikobelinsky123@gmail.com>
2022-09-29 22:28:20 -0500

    registro-ventas

--- apps/ventas_compras/ventas/urls.py
+++ apps/ventas_compras/ventas/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.ventas, name='ventas'),
-    path('registro-ventas/', views.ventas_registro, name='ventas_registro')
+    path('registro-ventas/', views.ventas_registro, name='ventas_registro'),
+    path('detalle/', views.ventas_registro_detalle, name='ventas_registro_detalle'),
+    path('nuevo-registro/', views.registro, name='registro'),
 ]
--- apps/ventas_compras/ventas/views.py
+++ apps/ventas_compras/ventas/views.py
@@ -6,3 +6,9 @@ def ventas(request):
 
 def ventas_registro(request):
     return render(request, 'ventas_registro.html')
+
+def registro(request):
+    return render(request, 'nuevo_registro.html')
+
+def ventas_registro_detalle(request):
+    return render(request, 'ventas/includes/_ventas_detalles.html')
