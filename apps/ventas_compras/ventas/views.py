00c6bffb761f6f52efecc44a55e7889e059bde67
loyfi <nikobelinsky123@gmail.com>
2022-10-19 00:34:57 -0500

    problemas en llaves foraneas

--- apps/ventas_compras/ventas/views.py
+++ apps/ventas_compras/ventas/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.db import transaction
 from django.contrib import messages
 from django.urls import reverse_lazy
@@ -7,13 +7,14 @@ from django.contrib import messages
 from django.db import transaction
 from django.shortcuts import render
 from django.db import transaction
-from apps.ventas_compras.ventas.forms import VentasForm
+from apps.ventas_compras.ventas.forms import DetallesVentasForm, VentasForm
+from apps.ventas_compras.ventas.models import Ventas, VentasDetalles
 # Create your views here.
 
 
 def ventas(request):
-    return render(request, 'ventas.html')
-
+    venta = VentasDetalles.objects.all()
+    return render(request, 'ventas.html', {'venta': venta})
 
 def ventas_registro(request):
     return render(request, 'ventas_registro.html')
@@ -23,16 +24,26 @@ def ventas_registro(request):
 def registro(request):
     if request.method == 'POST':
         form = VentasForm(request.POST)
-        if form.is_valid():
+        form2 = DetallesVentasForm(request.POST)
+        if form.is_valid() and form2.is_valid():
             with transaction.atomic():
+                print('form is valid')
                 form.save()
+                form2.save(commit=False)
+                form2.venta = form
+                form2.save()
                 messages.success(request, 'Registro exitoso')
                 return redirect(reverse_lazy('ventas'))
         else:
+            print('form is not valid')
             print(form.errors)
+            print(form2.errors)
             messages.error(request, 'Error al registrar')
     else:
+        print('form is not valid, reload')
         form = VentasForm()
+        form2 = DetallesVentasForm()
         # for field_name, field in form.fields.items():
         #     field.widget.attrs['disabled'] = False
-    return render(request, 'nuevo_registro.html', {'form': form})
+
+    return render(request, 'nuevo_registro.html', {'form': form, 'form2': form2})
