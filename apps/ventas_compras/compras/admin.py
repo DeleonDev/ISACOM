7298ca6628fdecd1bec8ab8af85349c5c2983cb7
loyfi <nikobelinsky123@gmail.com>
2022-11-12 00:06:46 -0600

    cambios penultimos

--- apps/ventas_compras/compras/admin.py
+++ apps/ventas_compras/compras/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from apps.ventas_compras.compras.models import Comisiones
+from apps.ventas_compras.compras.models import Comisiones, Compras
 
 # Register your models here.
 
 admin.site.register(Comisiones)
+admin.site.register(Compras)
--- apps/ventas_compras/compras/forms.py
+++ apps/ventas_compras/compras/forms.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 import re
 from datetime import date
 
-from apps.ventas_compras.compras.models import Comisiones
+from apps.ventas_compras.compras.models import Comisiones, Compras
 
 
 class ComisionesForm(forms.ModelForm):
@@ -55,4 +55,39 @@ class ComisionesForm(forms.ModelForm):
             field.widget.attrs['autocomplete'] = 'off'
             field.widget.attrs['accept'] = ''
         if field_name in ['fecha_inicio', 'fecha_fin']:
+                field.widget.attrs['onkeyup'] = ''
+                
+                
+class ComprasForm(forms.ModelForm):
+    class Meta:
+        model = Compras
+        fields = '__all__'
+        widgets = {
+            'fecha_entrega_estimada': forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'),
+            'fecha_entrega': forms.DateInput(attrs={'type': 'date'}, format='%Y-%m-%d'),
+        }
+        
+    def clean_orden_compra(self):
+        orden_compra = self.cleaned_data['orden_compra']
+        return orden_compra
+    
+    
+    def clean_fecha_entrega_estimada(self):
+        fecha_entrega_estimada = self.cleaned_data['fecha_entrega_estimada']
+        return fecha_entrega_estimada
+    
+    def clean_fecha_entrega(self):
+        fecha_entrega = self.cleaned_data['fecha_entrega']
+        return fecha_entrega
+    
+    def __init__(self, *args, **kwargs):
+        super(ComprasForm, self).__init__(*args, **kwargs)
+        self.fields['orden_compra'].empty_label = 'SELECCIONE'
+        for field_name, field in self.fields.items():
+            field.widget.attrs['onkeyup'] = 'javascript:this.value=this.value.toUpperCase()'
+            field.widget.attrs['class'] = 'form-control'
+            field.widget.attrs['placeholder'] = field.label
+            field.widget.attrs['autocomplete'] = 'off'
+            field.widget.attrs['accept'] = ''
+        if field_name in ['fecha_entrega_estimada', 'fecha_entrega']:
                 field.widget.attrs['onkeyup'] = ''
